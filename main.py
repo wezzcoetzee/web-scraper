a46c60d6ac4f404755e3c41547ea523d85fa851a
Wesley Coetzee <coetzeewr@gmail.com>
2023-06-27 20:59:23 +0200

    feat: Add write to text file

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from settings import TEXT_BASE_URL_TO_SCRAPE, TEXT_DATA_URL_TO_SCRAPE, CONTENT_TEXT_DATA_URL_TO_SCRAPE
+from models import UrlModel
+
+
+urls_to_scrape = [UrlModel(TEXT_DATA_URL_TO_SCRAPE, "Headers.txt"),
+                  UrlModel(CONTENT_TEXT_DATA_URL_TO_SCRAPE, "Content.txt")]
 
 
 def domain_scraper(url):
@@ -21,7 +26,7 @@ def domain_scraper(url):
     return ' '.join(text)
 
 
-def get_subpages(url):
+def get_subpages(url, file_name):
     response = requests.get(url)
 
     json_pattern = r'\[\{(.*?)\}\]'
@@ -36,12 +41,21 @@ def get_subpages(url):
                 link_suffix = sub_page['url']
                 link = f"{TEXT_BASE_URL_TO_SCRAPE}{link_suffix}"
                 print(link)
-                print(domain_scraper(link))
-            except AttributeError:
+                write_output(link, file_name)
+            except KeyError:
                 print("Not a valid subpage, skipping")
     else:
         print("No match found.")
 
 
-get_subpages(TEXT_DATA_URL_TO_SCRAPE)
-get_subpages(CONTENT_TEXT_DATA_URL_TO_SCRAPE)
+def write_output(link, file_name):
+    content = domain_scraper(link);
+    print(content)
+    file_path = f"output/{file_name}"  # Replace with the actual file path
+    file = open(file_path, 'w')
+    file.write('\n' * 5 + content)
+    file.close()
+
+
+for url_to_scrape in urls_to_scrape:
+    get_subpages(url_to_scrape.url, url_to_scrape.filename)
---
+++ models.py
@@ -0,0 +1,4 @@
+class UrlModel:
+    def __init__(self, url, filename):
+        self.url = url
+        self.filename = filename
